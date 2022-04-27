d8e5ded4862fa6459a0436a70fbc888c0eda13b0
Nuri <nuriddin7612@mail.ru>
2022-04-27 23:09:52 +0900

    Startup Name Generator

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:random_name/random_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
